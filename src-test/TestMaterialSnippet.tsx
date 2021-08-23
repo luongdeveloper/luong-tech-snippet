02213ebe719c53c719080cbafd1e140e28d994c6
luongdeveloper <luongdeveloper@gmail.com>
2021-08-24 00:01:03 +0700

    remove file test-html

--- src-test/TestMaterialSnippet.tsx
+++ src-test/TestMaterialSnippet.tsx
@@ -1,16 +1,16 @@
-import { Grid, makeStyles } from "@material-ui/core";
-import React, { useEffect, useState } from "react";
+// import { Grid, makeStyles } from "@material-ui/core";
+// import React, { useEffect, useState } from "react";
 
-type Props = {};
-const useStyle = makeStyles((theme) => ({}));
-function TestMaterialSnippet(props: Props) {
-    const classes = useStyle();
-    const [state, setState] = useState();
-    useEffect(() => {
-        return () => {};
-    }, []);
+// type Props = {};
+// const useStyle = makeStyles((theme) => ({}));
+// function TestMaterialSnippet(props: Props) {
+//     const classes = useStyle();
+//     const [state, setState] = useState();
+//     useEffect(() => {
+//         return () => {};
+//     }, []);
 
-    return <Grid></Grid>;
-}
+//     return <Grid></Grid>;
+// }
 
-export default React.memo(TestMaterialSnippet);
+// export default React.memo(TestMaterialSnippet);
